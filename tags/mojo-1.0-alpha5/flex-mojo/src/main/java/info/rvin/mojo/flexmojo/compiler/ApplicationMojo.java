b7a67eb4d8efad02b40a01ea3be27315f6051080
velo.br <velo.br@c794a531-bc48-0410-abaf-af34e93b40aa>
2008-04-01 02:01:36 +0000

    [maven-release-plugin]  copy for tag mojo-1.0-alpha5

---
+++ tags/mojo-1.0-alpha5/flex-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/ApplicationMojo.java
@@ -0,0 +1,78 @@
+package info.rvin.mojo.flexmojo.compiler;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import flex2.tools.oem.Application;
+import info.rvin.flexmojos.utilities.AppUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * @goal compile-swf
+ * @requiresDependencyResolution
+ * @phase compile
+ */
+public class ApplicationMojo extends AbstractFlexCompilerMojo<Application> {
+
+	/**
+	 * The file to be compiled
+	 * 
+	 * @parameter
+	 */
+	private String sourceFile;
+	/**
+	 * The file to be compiled
+	 */
+	private File source;
+
+	@Override
+	public void setUp() throws MojoExecutionException, MojoFailureException {
+		File sourceDirectory = new File(build.getSourceDirectory());
+		if (!sourceDirectory.exists()) {
+			throw new MojoExecutionException(
+					"Unable to found sourceDirectory: " + sourceDirectory);
+		}
+
+		source = AppUtil.resolveSourceFile(project, sourceFile);
+
+		if (source == null) {
+			throw new MojoExecutionException(
+					"Source file not expecified and no default found!");
+		}
+		if (!source.exists()) {
+			throw new MojoFailureException("Unable to find " + sourceFile);
+		}
+
+		// need to initialize builder before go super
+		try {
+			builder = new Application(source);
+		} catch (FileNotFoundException e) {
+			throw new MojoFailureException("Unable to find " + source);
+		}
+		super.setUp();
+
+		builder.setOutput(outputFile);
+
+	}
+
+}
