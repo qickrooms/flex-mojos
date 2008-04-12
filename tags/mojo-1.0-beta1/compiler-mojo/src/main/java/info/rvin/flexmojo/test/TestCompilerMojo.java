fcf939d4d5485ae084a6cab0bc4ae6829d31d8f2
velo.br <velo.br@c794a531-bc48-0410-abaf-af34e93b40aa>
2008-04-12 15:04:50 +0000

    [maven-release-plugin]  copy for tag mojo-1.0-beta1

---
+++ tags/mojo-1.0-beta1/compiler-mojo/src/main/java/info/rvin/flexmojo/test/TestCompilerMojo.java
@@ -0,0 +1,193 @@
+package info.rvin.flexmojo.test;
+
+import info.rvin.flexmojos.utilities.MavenUtils;
+import info.rvin.mojo.flexmojo.compiler.ApplicationMojo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal test-compile
+ * @requiresDependencyResolution
+ * @phase test
+ */
+public class TestCompilerMojo extends ApplicationMojo {
+
+	/**
+     * Set this to 'true' to bypass unit tests entirely.
+     * Its use is NOT RECOMMENDED, but quite convenient on occasion.
+     *
+     * @parameter expression="${maven.test.skip}"
+	 */
+	private boolean skipTests;
+
+	/**
+	 * @parameter
+	 */
+	private File template;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		File testFolder = new File(build.getTestSourceDirectory());
+		if (skipTests) {
+			getLog().warn("Skipping test phase.");
+		} else if (!testFolder.exists()) {
+			getLog().warn("Test folder not found" + testFolder);
+		} else {
+			super.execute();
+		}
+	}
+
+	@Override
+	public void setUp() throws MojoExecutionException, MojoFailureException {
+
+
+		File outputFolder = new File(build.getTestOutputDirectory());
+		if (!outputFolder.exists()) {
+			outputFolder.mkdirs();
+		}
+
+		File testFolder = new File(build.getTestSourceDirectory());
+		List<String> testClasses = getTestClasses(testFolder);
+
+		File testSourceFile;
+		try {
+			testSourceFile = generateTester(testClasses);
+		} catch (Exception e) {
+			throw new MojoExecutionException(
+					"Unable to generate tester class.", e);
+		}
+
+		sourceFile = null;
+		source = testSourceFile;
+
+		outputFile = new File(build.getTestOutputDirectory(), "TestRunner.swf");
+
+		super.setUp();
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<String> getTestClasses(File testFolder) {
+		Collection<File> testFiles = FileUtils.listFiles(testFolder,
+				new FileFileFilter() {
+					@Override
+					public boolean accept(File file) {
+						String name = file.getName();
+						if (name.startsWith("Test")
+								&& (name.endsWith(".as") || name
+										.endsWith(".mxml")))
+							return true;
+						return false;
+					}
+				}, DirectoryFileFilter.DIRECTORY);
+
+		List<String> testClasses = new ArrayList<String>();
+
+		int trimPoint = testFolder.getAbsolutePath().length() + 1;
+
+		for (File testFile : testFiles) {
+			String testClass = testFile.getAbsolutePath();
+			int endPoint = testClass.lastIndexOf('.');
+			testClass = testClass.substring(trimPoint, endPoint);
+			testClass = testClass.replace('/', '.'); // Unix OS
+			testClass = testClass.replace('\\', '.'); // Windows OS
+			testClasses.add(testClass);
+		}
+		return testClasses;
+	}
+
+	private File generateTester(List<String> testClasses) throws Exception {
+		// can't use velocity, got:
+		// java.io.InvalidClassException:
+		// org.apache.velocity.runtime.parser.node.ASTprocess; class invalid for
+		// deserialization
+
+		StringBuilder imports = new StringBuilder();
+
+		for (String testClass : testClasses) {
+			imports.append("import ");
+			imports.append(testClass);
+			imports.append(";");
+			imports.append('\n');
+		}
+
+		StringBuilder classes = new StringBuilder();
+
+		for (String testClass : testClasses) {
+			testClass = testClass.substring(testClass.lastIndexOf('.') + 1);
+			classes.append("testSuite.addTest( new TestSuite(");
+			classes.append(testClass);
+			classes.append(") );");
+			classes.append('\n');
+		}
+
+		InputStream templateSource = getTemplate();
+		String sourceString = IOUtils.toString(templateSource);
+		sourceString = sourceString.replace("$imports", imports);
+		sourceString = sourceString.replace("$testClasses", classes);
+		File testSourceFile = new File(build.getTestOutputDirectory(),
+				"TestRunner.mxml");
+		FileWriter fileWriter = new FileWriter(testSourceFile);
+		IOUtils.write(sourceString, fileWriter);
+		fileWriter.flush();
+		fileWriter.close();
+		return testSourceFile;
+	}
+
+	private InputStream getTemplate() throws MojoExecutionException {
+		if (template == null) {
+			return getClass().getResourceAsStream("/test/TestRunner.vm");
+		} else if (!template.exists()) {
+			throw new MojoExecutionException("Template file not found: "
+					+ template);
+		} else {
+			try {
+				return new FileInputStream(template);
+			} catch (FileNotFoundException e) {
+				//Never should happen
+				throw new MojoExecutionException("Error reading template file",
+						e);
+			}
+		}
+	}
+
+	@Override
+	protected void configure() throws MojoExecutionException {
+		super.configure();
+
+		// Remove all libraries
+		configuration.setExternalLibraryPath(null);
+		configuration.setIncludes(null);
+		configuration.setRuntimeSharedLibraries(null);
+
+		// Set all dependencies as merged
+		configuration.setLibraryPath(getDependenciesPath("compile"));
+		configuration.addLibraryPath(getDependenciesPath("merged"));
+		configuration.addLibraryPath(getDependenciesPath("external"));
+		configuration.addLibraryPath(getResourcesBundles());
+		configuration.addLibraryPath(getDependenciesPath("internal"));
+		configuration.addLibraryPath(getDependenciesPath("rsl"));
+		// and add test libraries
+		configuration.addLibraryPath(getDependenciesPath("test"));
+
+		configuration.addSourcePath(new File[] { new File(build
+				.getTestOutputDirectory()) });
+		configuration.addSourcePath(MavenUtils.getTestSourcePaths(build));
+		configuration.allowSourcePathOverlap(true);
+
+	}
+
+}
---
+++ tags/mojo-1.0-beta1/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/LibraryMojo.java
@@ -0,0 +1,235 @@
+package info.rvin.mojo.flexmojo.compiler;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static info.rvin.flexmojos.utilities.MavenUtils.resolveArtifact;
+import flex2.tools.oem.Library;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * @goal compile-swc
+ * @requiresDependencyResolution
+ * @phase compile
+ */
+public class LibraryMojo extends AbstractFlexCompilerMojo<Library> {
+
+	/**
+	 * Enable or disable the computation of a digest for the created swf
+	 * library. This is equivalent to using the
+	 * <code>compiler.computDigest</code> in the compc compiler.
+	 * 
+	 * @parameter default-value="true"
+	 */
+	private boolean computeDigest;
+
+	/**
+	 * This is the equilvalent of the <code>include-classes</code> option of
+	 * the compc compiler.
+	 * 
+	 * @parameter
+	 */
+	private String[] includeClasses;
+
+	/**
+	 * This is equilvalent to the <code>include-file</code> option of the
+	 * compc compiler.
+	 * 
+	 * @parameter
+	 */
+	private File[] includeFiles;
+
+	/**
+	 * This is equilvalent to the <code>include-namespaces</code> option of
+	 * the compc compiler.
+	 * 
+	 * @parameter
+	 */
+	private String[] includeNamespaces;
+
+	/**
+	 * This is equilvalent to the <code>include-resource-bundles</code> option
+	 * of the compc compiler.
+	 * 
+	 * @parameter
+	 */
+	private String[] includeResourceBundles;
+
+	/**
+	 * @parameter
+	 */
+	private MavenArtifact[] includeResourceBundlesArtifact;
+
+	/**
+	 * This is the equilvalent of the <code>include-sources</code> option of
+	 * the compc compiler.
+	 * 
+	 * @parameter
+	 */
+	private File[] includeSources;
+
+	@Override
+	public void setUp() throws MojoExecutionException, MojoFailureException {
+		// need to initialize builder before go super
+		builder = new Library();
+		super.setUp();
+
+		builder.setOutput(outputFile);
+
+		if (checkNullOrEmpty(includeClasses) && checkNullOrEmpty(includeFiles)
+				&& checkNullOrEmpty(includeNamespaces)
+				&& checkNullOrEmpty(includeResourceBundles)
+				&& checkNullOrEmpty(includeResourceBundlesArtifact)
+				&& checkNullOrEmpty(includeSources)) {
+			throw new MojoExecutionException("Nothing to be included.");
+		}
+
+		if (!checkNullOrEmpty(includeClasses)) {
+			for (String asClass : includeClasses) {
+				builder.addComponent(asClass);
+			}
+		}
+
+		if (!checkNullOrEmpty(includeFiles)) {
+			for (File file : includeFiles) {
+				if (file == null) {
+					throw new MojoFailureException("Cannot include a null file");
+				}
+				if (!file.exists()) {
+					throw new MojoFailureException("File " + file.getName()
+							+ " not found");
+				}
+				builder.addArchiveFile(file.getName(), file);
+			}
+		}
+
+		if (!checkNullOrEmpty(includeNamespaces)) {
+			for (String uri : includeNamespaces) {
+				try {
+					builder.addComponent(new URI(uri));
+				} catch (URISyntaxException e) {
+					throw new MojoExecutionException("Invalid URI " + uri, e);
+				}
+			}
+		}
+
+		if (!checkNullOrEmpty(includeResourceBundles)) {
+			for (String rb : includeResourceBundles) {
+				builder.addResourceBundle(rb);
+			}
+		}
+
+		if (!checkNullOrEmpty(includeResourceBundlesArtifact)) {
+			for (MavenArtifact mvnArtifact : includeResourceBundlesArtifact) {
+				Artifact artifact = artifactFactory
+						.createArtifactWithClassifier(mvnArtifact.getGroupId(),
+								mvnArtifact.getArtifactId(), mvnArtifact
+										.getVersion(), "properties",
+								"resource-bundle");
+				resolveArtifact(artifact, resolver, localRepository,
+						remoteRepositories);
+				String bundleFile;
+				try {
+					bundleFile = FileUtils.readFileToString(artifact.getFile());
+				} catch (IOException e) {
+					throw new MojoExecutionException(
+							"Ocorreu um erro ao ler o artefato " + artifact, e);
+				}
+				String[] bundles = bundleFile.split(" ");
+				for (String bundle : bundles) {
+					builder.addResourceBundle(bundle);
+				}
+			}
+		}
+
+		if (!checkNullOrEmpty(includeSources)) {
+			for (File file : includeSources) {
+				if (file == null) {
+					throw new MojoFailureException("Cannot include a null file");
+				}
+				if (!file.exists()) {
+					throw new MojoFailureException("File " + file.getName()
+							+ " not found");
+				}
+				builder.addComponent(file);
+			}
+		}
+
+		configuration.enableDigestComputation(computeDigest);
+	}
+
+	private boolean checkNullOrEmpty(Object[] includeClasses) {
+		if (includeClasses == null) {
+			return true;
+		}
+
+		if (includeClasses.length == 0) {
+			return false;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected void writeResourceBundle(String[] bundles, String locale,
+			File localePath) throws MojoExecutionException {
+		getLog().info("Generating resource-bundle for " + locale);
+
+		Library localized = new Library();
+		localized.setConfiguration(configuration);
+
+		localized.setLogger(new CompileLogger(getLog()));
+
+		configuration.addLibraryPath(new File[] { outputFile });
+		configuration.setLocale(new String[] { locale });
+		configuration.setSourcePath(new File[] { localePath });
+		for (String bundle : bundles) {
+			localized.addResourceBundle(bundle);
+		}
+		configuration.addLibraryPath(getResourcesBundles());
+
+		File output = new File(build.getDirectory(), project.getArtifactId()
+				+ "-" + project.getVersion() + "-" + locale + ".swc");
+
+		localized.setOutput(output);
+		long bytes;
+		try {
+			bytes = localized.build(false);
+		} catch (IOException e) {
+			throw new MojoExecutionException(
+					"Unable to compile resource bundle", e);
+		}
+		if (bytes == 0) {
+			throw new MojoExecutionException(
+					"Unable to compile resource bundle");
+		}
+
+		projectHelper
+				.attachArtifact(project, "resource-bundle", locale, output);
+	}
+
+}
