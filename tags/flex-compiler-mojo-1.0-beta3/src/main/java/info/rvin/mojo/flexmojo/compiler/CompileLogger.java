c942f66c94b4603bcf45c736a23f15a172235bef
velo.br <velo.br@c794a531-bc48-0410-abaf-af34e93b40aa>
2008-04-22 00:25:25 +0000

    [maven-release-plugin]  copy for tag flex-compiler-mojo-1.0-beta3

---
+++ tags/flex-compiler-mojo-1.0-beta3/src/main/java/info/rvin/mojo/flexmojo/compiler/CompileLogger.java
@@ -0,0 +1,49 @@
+package info.rvin.mojo.flexmojo.compiler;
+
+import org.apache.maven.plugin.logging.Log;
+
+import flex2.tools.oem.Logger;
+import flex2.tools.oem.Message;
+
+public class CompileLogger implements Logger {
+
+	private Log log;
+
+	public CompileLogger(Log log) {
+		this.log = log;
+	}
+
+	public void log(Message msg, int errorCode, String source) {
+		if (Message.ERROR.equals(msg.getLevel())) {
+			log.error(getMessage(msg, source));
+		} else if (Message.INFO.equals(msg.getLevel())) {
+			log.info(getMessage(msg, source));
+		} else if (Message.WARNING.equals(msg.getLevel())) {
+			log.warn(getMessage(msg, source));
+		}
+	}
+
+	private String getMessage(Message msg, String source) {
+		StringBuilder sb = new StringBuilder();
+
+		if (msg.getPath() != null) {
+			sb.append(msg.getPath());
+			sb.append(':');
+			sb.append('[');
+			sb.append(msg.getLine());
+			sb.append(',');
+			sb.append(msg.getColumn());
+			sb.append(']');
+			sb.append(' ');
+		}
+
+		sb.append(msg.toString());
+
+		if (source != null) {
+			sb.append(source);
+		}
+
+		return sb.toString();
+	}
+
+}
